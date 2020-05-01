2d1ba2387cef38f2d01a4702d706d56fac900303
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-05-01 15:54:16 +0200

    update prefixes

--- jsonld-context.js
+++ jsonld-context.js
@@ -1,16 +1,17 @@
 export default {
-    "muAccount": "http://mu.semte.ch/vocabularies/account/",
-    "dataRequest": "http://data.lblod.info/vocabularies/data-request/",
-    "schema": "http://schema.org/memberOf",
-    "requester": "dataRequest:requester",
-    "dcterms": "http://purl.org/dc/terms/",
-    "skos": "http://www.w3.org/2004/02/skos/core#",
-    "uri": {
-      "@type": "@id",
-      "@id": "@id"
-    },
-    "key": "muAccount:key",
-    "person": "dataRequest:requestedPerson",
-    "rrn": "skos:notation",
-    "organization": "schema:memberOf"
+  "muAccount": "http://mu.semte.ch/vocabularies/account/",
+  "dataRequest": "http://data.lblod.info/vocabularies/data-request/",
+  "schema": "http://schema.org/",
+  "foaf": "http://xmlns.com/foaf/0.1/",
+  "dcterms": "http://purl.org/dc/terms/",
+  "skos": "http://www.w3.org/2004/02/skos/core#",
+  "uri": {
+    "@type": "@id",
+    "@id": "@id"
+  },
+  "key": "muAccount:key",
+  "person": "dataRequest:requestedPerson",
+  "rrn": "skos:notation",
+  "organization": "schema:memberOf",
+  "requester": "dataRequest:requester"
 };
