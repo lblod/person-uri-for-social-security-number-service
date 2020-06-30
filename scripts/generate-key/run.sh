642e15f68418e876e2862334077c9dcf634d3efd
Claire Lovisa <lovisa.claire@gmail.com>
2020-06-30 10:40:37 +0200

    Capitalize variable names in script

--- scripts/generate-key/run.sh
+++ scripts/generate-key/run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 if [ "$#" -ne 4 ]; then
-  echo "The number of parameters is not correct. You need to provide the following: ./rush.sh vendorUri bestuurseenheiduri key passwordSalt"
+  echo "The number of parameters is not correct. You need to provide the following: ./rush.sh VENDOR_URI BESTUURSEENHEID_URI key PASSWORD_SALT"
   exit 1
 fi
 
-vendorUri=$1
-bestuurseenheiduri=$2
-key=$3
-passwordSalt=$4
-agentUuid=$(cat /proc/sys/kernel/random/uuid)
-accountUuid=$(cat /proc/sys/kernel/random/uuid)
-aclsUuid=$(cat /proc/sys/kernel/random/uuid)
-informationResourceUuid=$(cat /proc/sys/kernel/random/uuid)
+VENDOR_URI=$1
+BESTUURSEENHEID_URI=$2
+KEY=$3
+PASSWORD_SALT=$4
+AGENT_UUID=$(cat /proc/sys/kernel/random/uuid)
+ACCOUNT_UUID=$(cat /proc/sys/kernel/random/uuid)
+ACLS_UUID=$(cat /proc/sys/kernel/random/uuid)
+INFORMATION_RESOURCE_UUID=$(cat /proc/sys/kernel/random/uuid)
 
-query="PREFIX mandaat: <http://data.vlaanderen.be/ns/mandaat#>
+QUERY="PREFIX mandaat: <http://data.vlaanderen.be/ns/mandaat#>
 PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
 PREFIX rrn: <http://data.lblod.info/vocabularies/rrn/>
 PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
@@ -28,35 +28,33 @@ PREFIX dcterms: <http://purl.org/dc/terms/>
 
 INSERT {
   GRAPH <http://mu.semte.ch/graphs/ssn-access-control> {
-    <$vendorUri> acl:member <http://data.lblod.info/agents/$agentUuid>.
+    <$VENDOR_URI> acl:member <http://data.lblod.info/agents/$AGENT_UUID>.
 
-    <http://data.lblod.info/agents/$agentUuid> a muAccount:SSNAgent.
-    <http://data.lblod.info/agents/$agentUuid> foaf:account <http://data.lblod.info/accounts/$accountUuid>.
+    <http://data.lblod.info/agents/$AGENT_UUID> a muAccount:SSNAgent.
+    <http://data.lblod.info/agents/$AGENT_UUID> foaf:account <http://data.lblod.info/accounts/$ACCOUNT_UUID>.
 
-   <http://data.lblod.info/accounts/$accountUuid> a foaf:OnlineAccount.
-   # the non-hashed-key is: \"$key\"
-   # salt:\"$passwordSalt\"
-   # muAccount:key SHA512(\"$key$passwordSalt\")
-   <http://data.lblod.info/accounts/$accountUuid> muAccount:key ?shaKey.
+   <http://data.lblod.info/accounts/$ACCOUNT_UUID> a foaf:OnlineAccount.
+   # the non-hashed-key is: \"$KEY\"
+   # salt:\"$PASSWORD_SALT\"
+   # muAccount:key SHA512(\"$KEY$PASSWORD_SALT\")
+   <http://data.lblod.info/accounts/$ACCOUNT_UUID> muAccount:key ?shaKey.
 
-   <http://data.lblod.info/acls/$aclsUuid> acl:agent <http://data.lblod.info/agents/$agentUuid>.
-   <http://data.lblod.info/acls/$aclsUuid> a acl:Authorization.
-   <http://data.lblod.info/acls/$aclsUuid> acl:mode <http://data.lblod.info/codelists/access-modes/read>.
-   <http://data.lblod.info/acls/$aclsUuid> acl:accessTo <http://data.lblod.info/information-resources/$informationResourceUuid>.
+   <http://data.lblod.info/acls/$ACLS_UUID> acl:agent <http://data.lblod.info/agents/$AGENT_UUID>.
+   <http://data.lblod.info/acls/$ACLS_UUID> a acl:Authorization.
+   <http://data.lblod.info/acls/$ACLS_UUID> acl:mode <http://data.lblod.info/codelists/access-modes/read>.
+   <http://data.lblod.info/acls/$ACLS_UUID> acl:accessTo <http://data.lblod.info/information-resources/$INFORMATION_RESOURCE_UUID>.
 
-   <http://data.lblod.info/information-resources/$informationResourceUuid> a muAccount:SSNAccess.
-   <http://data.lblod.info/information-resources/$informationResourceUuid> dcterms:subject <$bestuurseenheiduri>.
+   <http://data.lblod.info/information-resources/$INFORMATION_RESOURCE_UUID> a muAccount:SSNAccess.
+   <http://data.lblod.info/information-resources/$INFORMATION_RESOURCE_UUID> dcterms:subject <$BESTUURSEENHEID_URI>.
   }
 } WHERE {
-  { SELECT SHA512(\"$key$passwordSalt\") as ?shaKey WHERE {} }.
+  { SELECT SHA512(\"$KEY$PASSWORD_SALT\") as ?shaKey WHERE {} }.
 }"
 
-timestamp=`date "+%Y%m%d%H%M%S"`
-filename=$timestamp"-generate-key.sparql"
+TIMESTAMP=`date "+%Y%m%d%H%M%S"`
+FILENAME=$TIMESTAMP"-generate-key.sparql"
 
-echo "Creating migration with name $filename"
+echo "Creating migration with name $FILENAME"
 mkdir -p /data/app/config/migrations/
 cd /data/app/config/migrations/
-#touch $filename
-#echo "config/migrations/$filename" 
-echo "$query" >> $filename
+echo "$QUERY" >> $FILENAME
